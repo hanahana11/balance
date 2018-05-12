d2602374c74ef8b809fa51f2155ccd3ddded4789
末利　花 <hana87kotaro@i.softbank.jp>
2018-05-12 20:13:02 +0900

    マジ卍

--- balance/ViewController.swift
+++ balance/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var label: UILabel!
+    
+    var count: Float = 0.0
+    
+    var timer: Timer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +25,21 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func start() {
+        
+        if !timer.isValid {
+            timer = Timer.scheduledTimer(timeInterval: 0.01, target: self,
+                                         selector: #selector(self.up),
+                                         userInfo: nil,
+                                         repeats: true
+            )
+        }
+    }
 
+    @IBAction func stop() {
+        
+    }
 
 }
 
